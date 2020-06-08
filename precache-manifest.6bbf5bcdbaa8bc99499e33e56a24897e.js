f6995fcfd227460d6edc24af653aedc4e92ba176
AdityaKG-169 <f20190338@goa.bits-pilani.ac.in>
2020-06-08 17:59:06 +0530

    Updates

---
+++ precache-manifest.6bbf5bcdbaa8bc99499e33e56a24897e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ab3fbbc6d0d00d592b5a6475808561",
+    "url": "/adityakrishnagupta.github.io/index.html"
+  },
+  {
+    "revision": "4d9b9dd2aa8b99789c1b",
+    "url": "/adityakrishnagupta.github.io/static/css/main.376b3ba5.chunk.css"
+  },
+  {
+    "revision": "bea834aff6511e61c72f",
+    "url": "/adityakrishnagupta.github.io/static/js/2.465cb5bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adityakrishnagupta.github.io/static/js/2.465cb5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9b9dd2aa8b99789c1b",
+    "url": "/adityakrishnagupta.github.io/static/js/main.c9c2d0b4.chunk.js"
+  },
+  {
+    "revision": "dc4cb02b181a33256f29",
+    "url": "/adityakrishnagupta.github.io/static/js/runtime-main.8b1231da.js"
+  },
+  {
+    "revision": "382bcb41d928f5af2cd1335011a38a35",
+    "url": "/adityakrishnagupta.github.io/static/media/body.382bcb41.svg"
+  },
+  {
+    "revision": "7aec47e8dafd49e202d99774b0927a8f",
+    "url": "/adityakrishnagupta.github.io/static/media/logo.7aec47e8.svg"
+  }
+]);
